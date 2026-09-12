42937378039d759c3b88cfb53a021e8e263380d7
demshinal-stack <demshinal@gmail.com>
2026-09-12 10:47:16 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Атлас — офлайн-кеш и обновления.
    Версию поднимайте вместе с APP_VERSION в приложении. */
-const VERSION = "atlas-2.3";
+const VERSION = "atlas-2.4";
 const SHELL = [
   "./",
   "./index.html",
